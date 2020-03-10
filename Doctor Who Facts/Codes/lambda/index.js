d86e0d9791377f7a5d150eebc4bfd3122d95c572
Siddharth Kekre <siddharthkekre@iSiddharthMac.local>
2020-03-10 17:33:02 +0530

    Doctor Who Facts

---
+++ Doctor Who Facts/Codes/lambda/index.js
@@ -0,0 +1,132 @@
+// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+// session persistence, api calls, and more.
+const Alexa = require('ask-sdk-core');
+
+const  facts = 
+    [
+    'The original Daleks were controlled from the inside by short heighted operators.' , 
+    'The theme music was originally composed by Ron Grainer and created by Delia Derbyshire at the BBC Radiophonic Workshop. But over the years, only its iteration is being used.' , 
+    'The distinctive Tardis sound effect was originally created by simply rubbing the bass strings of a piano with a key. This sound was then modified by the BBC Radiophonic Workshop.' , 
+    'The Doctor has visited many different planets, but his home planet is called Gallifrey.' , 
+    'The Doctor actually is a doctor! Once a man asked him, Listen, are you really a medical doctor? He replied, Yes I think I was once. I think I took a degree once in Glasgow, 1888 I think.' , 
+    'Bow ties are cool, that is what Matt Smith said in his first appearance as the Doctor. Bow tie sales shot up by 94% within a month at one high street store!' , 
+    'Even though it is the law that Time Lords can regenerate only 12 times, yet the Doctor is differnet as he she is the the Timeless Child and also is the genetic code for all the Time Lords. Spoilers!' , 
+    'The Doctors real name remains a complete mystery to all but a very small number of individuals know it including The Master, River Song and Clara Oswald.' , 'During his travels throughout time the Doctor has met and in some cases befriended plenty of historical figures, including: Leonardo da Vinci, William Shakespeare, Albert Einstein, Charles Dickens, Queen Victoria etc.' , 'Asteroid 3325, a main belt asteroid discovered in 1984, is named Tardis after the Doctors time/space machine.'
+    ]
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Welcome to Doctor Who Facts, you can say Tell Me a Fact or Give me a Fact. Which would you like to try?';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const DrWhoFactsIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'DrWhoFactsIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = facts[Math.floor(Math.random() * facts.length)];
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say give me some facinating facts! and I will give some great Doctor Who Facts?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.stack}`);
+        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        DrWhoFactsIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    )
+    .addErrorHandlers(
+        ErrorHandler,
+    )
+    .lambda();
